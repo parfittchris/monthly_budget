e7be12a741f7233afe9f0d2e39404d22f7ce0157
Chris Parfitt <parfitt.chris7@gmail.com>
2020-03-12 22:47:40 -0400

    Updates

---
+++ precache-manifest.751d1d4d48cc2e0e0b1a9033d6b2176e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0590a412463cd69fa93daf28d9d63931",
+    "url": "/monthly_budget/index.html"
+  },
+  {
+    "revision": "bac0aff638289979301f",
+    "url": "/monthly_budget/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "591b6ae5b9d2a8ce0fcb",
+    "url": "/monthly_budget/static/css/main.dc5a0058.chunk.css"
+  },
+  {
+    "revision": "bac0aff638289979301f",
+    "url": "/monthly_budget/static/js/2.40192707.chunk.js"
+  },
+  {
+    "revision": "5e41a8f21a28df9b0f9c1bc479bb6432",
+    "url": "/monthly_budget/static/js/2.40192707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591b6ae5b9d2a8ce0fcb",
+    "url": "/monthly_budget/static/js/main.4cd67fe1.chunk.js"
+  },
+  {
+    "revision": "5bfb95cd41f921b1374a",
+    "url": "/monthly_budget/static/js/runtime-main.25045692.js"
+  },
+  {
+    "revision": "f38517c11a9ad5a91ed00309d6afad80",
+    "url": "/monthly_budget/static/media/appBackground.f38517c1.jpeg"
+  }
+]);
