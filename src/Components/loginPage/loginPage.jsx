59223710058111ebb179f54a658588053eacb5a3
Chris Parfitt <parfitt.chris7@gmail.com>
2020-03-13 00:29:29 -0400

    finish styling

--- src/Components/loginPage/loginPage.jsx
+++ src/Components/loginPage/loginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
+import './loginPage.css';
 
 class loginPage extends React.Component {
 
@@ -18,13 +19,19 @@ class loginPage extends React.Component {
      }
 
      return (
-       <GoogleLogin
-         clientId='428362583003-j1lujcao0smc555smf6b0ck703c22h8n.apps.googleusercontent.com'
-         buttonText='Login'
-         onSuccess={redirect}
-         onFailure={responseGoogle}
-         cookiePolicy={'single_host_origin'}
-       />
+       <div id="loginPage">
+         <h1 className="loginText">Welcome to Budget Monthly!</h1>
+         <h3 className="loginText">Please log in below using your Google Account</h3>
+         <div id='loginBtn'>
+          <GoogleLogin
+            clientId='428362583003-j1lujcao0smc555smf6b0ck703c22h8n.apps.googleusercontent.com'
+            buttonText='Login'
+            onSuccess={redirect}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+         </div>
+       </div>
      );
     }
 }
